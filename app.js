06508c0bec019784ec3acf926e7cb6920fe42718
Hristo <hristo_ixa@abv.bg>
2021-05-04 19:38:18 +0300

    Fix div positioning, remove background image and replace it with a solid color

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+var totalBudget = 1628;
 var d = new Date;
 var month = d.getMonth();
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
     "October", "November", "December"];
 
-document.getElementById("totals").innerText = "Total budget for " + getMonth();
-
+document.getElementById("totals-banner").innerText = "Available budget in " + getMonth();
+document.getElementById("total-budget").innerText = totalBudget.toString();
 
 
 function getMonth() {
